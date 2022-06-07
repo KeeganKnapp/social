506fc11591027f39d396d953dfd54ebea2f7f91b
Keegan <keegan.a.knapP@gmail.com>
2022-06-06 21:15:46 -0400

    add user creation and view users

--- Social2/Controllers/HomeController.cs
+++ Social2/Controllers/HomeController.cs
@@ -4,9 +4,27 @@ namespace Social2.Controllers
 {
     public class HomeController : Controller
     {
+        private static List<User> users = new List<User>();
         public ViewResult Index()
         {
+            if (Request.Method.ToUpper() == "POST")
+            {
+                string username = Request.Form["username"];
+                string password = Request.Form["password"];
+                User user = new User();
+                user.UserName = username;
+                user.Password = password;
+                users.Add(user);
+            }
+     
             return View();
+
+        }
+
+        public ViewResult Users()
+        {
+            
+            return View(users);
         }
     }
 }
