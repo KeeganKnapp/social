45fa2bbdc9258c7f734bf80803626c3da89bd36e
Keegan <keegan.a.knapP@gmail.com>
2022-06-06 20:00:01 -0400

    add project files

---
+++ Social2/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Social2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Models.SquareResult result = new Models.SquareResult();
+
+            string input = Request.Query["input"];
+            int inputAsInt = int.Parse(input);
+            int inputSquared = inputAsInt * inputAsInt;
+
+            result.SetVal(inputSquared);
+            return View(result);
+        }
+    }
+}
---
+++ Social2/Models/SquareResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Social2.Models
+{
+    public class SquareResult
+    {
+        public int Val { get; set; }
+     
+    }
+}
